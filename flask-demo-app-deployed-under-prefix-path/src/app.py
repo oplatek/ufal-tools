ead08d02d72b8bd416603691cdbe4f5891e2ef2c
Ondrej Platek <ondrej.platek@seznam.cz>
2023-01-31 21:22:17 +0100

    WIP

--- flask-demo-app-deployed-under-prefix-path/src/app.py
+++ flask-demo-app-deployed-under-prefix-path/src/app.py
@@ -1,6 +1,27 @@
 from flask import Flask, url_for, render_template
+import os
 
-app = Flask(__name__)
+
+class DevelopmentProxyFix:
+    """Source https://stackoverflow.com/a/63429418/2171485"""
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ["SCRIPT_NAME"] = os.environ.get("SCRIPT_NAME")
+        print(environ["SCRIPT_NAME"], flush=True)
+        # environ["wsgi.url_scheme"] = "https"
+        return self.app(environ, start_response)
+
+def create_app():
+    app = Flask(__name__)
+    if app.config.get("ENV") == "development":
+        print(app.config["ENV"])
+        app.wsgi_app = DevelopmentProxyFix(app.wsgi_app)
+    return app
+
+
+app = create_app()
 
 @app.route("/")
 @app.route("/index")
